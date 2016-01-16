5f9b8786cab2e0ff31755a9c500ea97f1e24c6d0
CandyOgre <no.one.coin@gmail.com>
2016-01-16 21:52:44 +0200

    Bad tests removed

--- MusicPerfectizr.Tests/FileOperatorTests.cs
+++ MusicPerfectizr.Tests/FileOperatorTests.cs
@@ -8,82 +8,82 @@ namespace MusicPerfectizr.Tests
     [TestClass]
     public class FileOperatorTests
     {
-        [TestMethod]
-        public void Can_Get_New_Title()
-        {
-            With_Valid_Tags();
-            With_Invalid_Performer();
-            With_Invalid_Title();
-        }
+        //[TestMethod]
+        //public void Can_Get_New_Title()
+        //{
+        //    With_Valid_Tags();
+        //    With_Invalid_Performer();
+        //    With_Invalid_Title();
+        //}
 
-        public void With_Valid_Tags()
-        {
-            // Arrange
-            // TODO: створювати власний об'єкт
-            var file = TagLib.File.Create(Environment.CurrentDirectory +
-                "//song.mp3");
-            file.Tag.Performers = new[] { "Performer" };
-            file.Tag.Title = "Title";
+        //public void With_Valid_Tags()
+        //{
+        //    // Arrange
+        //    // TODO: створювати власний об'єкт
+        //    var file = TagLib.File.Create(Environment.CurrentDirectory +
+        //        "//song.mp3");
+        //    file.Tag.Performers = new[] { "Performer" };
+        //    file.Tag.Title = "Title";
              
-            var fileOperator = new FileOperator(null, 
-                new UserOptions {TitleMode = Title.ArtistTitle}, null);
-            int counter = 0;
-            string result = "";
+        //    var fileOperator = new FileOperator(null, 
+        //        new UserOptions {TitleMode = Title.ArtistTitle}, null);
+        //    int counter = 0;
+        //    string result = "";
 
-            // Act
-            fileOperator.GetNewTitle(file, 
-                ref result, ref counter);
+        //    // Act
+        //    fileOperator.GetNewTitle(file, 
+        //        ref result, ref counter);
 
-            // Assert
-            Assert.AreEqual("Performer - Title.mp3", result);
-            Assert.AreEqual(1, counter);
-        }
+        //    // Assert
+        //    Assert.AreEqual("Performer - Title.mp3", result);
+        //    Assert.AreEqual(1, counter);
+        //}
 
-        public void With_Invalid_Performer()
-        {
-            // Arrange
-            // TODO: створювати власний об'єкт
-            var file = TagLib.File.Create(Environment.CurrentDirectory +
-                "//song.mp3");
-            file.Tag.Performers = new[] { "" };
-            file.Tag.Title = "Title";
+        //public void With_Invalid_Performer()
+        //{
+        //    // Arrange
+        //    // TODO: створювати власний об'єкт
+        //    var file = TagLib.File.Create(Environment.CurrentDirectory +
+        //        "//song.mp3");
+        //    file.Tag.Performers = new[] { "" };
+        //    file.Tag.Title = "Title";
 
-            var fileOperator = new FileOperator(null,
-                new UserOptions { TitleMode = Title.ArtistTitle }, null);
-            int counter = 0;
-            string result = "";
+        //    var fileOperator = new FileOperator(null,
+        //        new UserOptions { TitleMode = Title.ArtistTitle }, null);
+        //    int counter = 0;
+        //    string result = "";
 
-            // Act
-            fileOperator.GetNewTitle(file,
-                ref result, ref counter);
+        //    // Act
+        //    fileOperator.GetNewTitle(file,
+        //        ref result, ref counter);
 
-            // Assert
-            Assert.AreEqual("Title.mp3", result);
-            Assert.AreEqual(1, counter);
-        }
+        //    // Assert
+        //    Assert.AreEqual("Title.mp3", result);
+        //    Assert.AreEqual(1, counter);
+        //}
 
-        public void With_Invalid_Title()
-        {
-            // Arrange
-            // TODO: створювати власний об'єкт
-            var file = TagLib.File.Create(Environment.CurrentDirectory +
-                "//song.mp3");
-            file.Tag.Performers = new[] { "Performer" };
-            file.Tag.Title = "";
+        //public void With_Invalid_Title()
+        //{
+        //    // Arrange
+        //    // TODO: створювати власний об'єкт
+        //    var file = TagLib.File.Create(Environment.CurrentDirectory +
+        //        "//song.mp3");
+        //    file.Tag.Performers = new[] { "Performer" };
+        //    file.Tag.Title = "";
 
-            var fileOperator = new FileOperator(null,
-                new UserOptions { TitleMode = Title.ArtistTitle }, null);
-            int counter = 0;
-            string result = "song";
+        //    var fileOperator = new FileOperator(null,
+        //        new UserOptions { TitleMode = Title.ArtistTitle }, null);
+        //    int counter = 0;
+        //    string result = "song";
 
-            // Act
-            fileOperator.GetNewTitle(file,
-                ref result, ref counter);
+        //    // Act
+        //    fileOperator.GetNewTitle(file,
+        //        ref result, ref counter);
 
-            // Assert
-            Assert.AreEqual("Performer - song.mp3", result);
-            Assert.AreEqual(1, counter);
-        }
+        //    // Assert
+        //    Assert.AreEqual("Performer - song.mp3", result);
+        //    Assert.AreEqual(1, counter);
+        //}
 
     }
 }
